1758d17773d35e7d2bbf8a0e97956672c1403553
Tracari <105556091+Tracari@users.noreply.github.com>
2022-07-12 01:19:34 -0400

    create a flask route

---
+++ app.py
@@ -0,0 +1,11 @@
+# import Flask dependency
+from flask import Flask
+
+# create a new flask app instance
+app = Flask(__name__)
+
+# create a Flask route
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
